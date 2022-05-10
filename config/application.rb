072a2b1a8579bb2deba398522fc838915e108b5f
John Rose <johnedmundrose@gmail.com>
2022-05-09 21:35:11 -0400

    Eager load in dev, eager load lib/

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,12 @@ Bundler.require(*Rails.groups)
 module Mmagic
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
+
+    # https://guides.rubyonrails.org/autoloading_and_reloading_constants.html#load_path
+    config.add_autoload_paths_to_load_path = false
+
+    config.eager_load_paths << Rails.root.join("lib")
 
     # Configuration for the application, engines, and railties goes here.
     #
