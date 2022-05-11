458f1fec553e956fabc988069e06db0224a44a23
John Rose <johnedmundrose@gmail.com>
2022-05-11 03:10:09 -0400

    Fix hierarchy rebuild, centralize db nuking

--- app/lib/seeding/seeder.rb
+++ app/lib/seeding/seeder.rb
@@ -7,6 +7,7 @@ class Seeding
     DEFAULT_NUM_SUBREGIONS_PER_REGION = 7..7 # 5..15
     DEFAULT_NUM_UNITS_PER_SUBREGION = 9..9 # 25..25 # 10..25
     BATCH_IMPORT_SIZE = 2000
+    DEFAULT_SENSORS_PER_PLACE = 1..5
     MONITOR_MAGIC_CORPORATION_NAME = "Monitor Magic, Inc."
 
     def initialize
@@ -16,13 +17,38 @@ class Seeding
     end
 
     def seed!
+      nuke_all!
       seed_places!
+      Seeding::Seeder::SensorTypes.seed!
+      seed_sensors!
       seed_checklists!
+    end
+
+    def nuke_all!
+      log(" --- nuking old data ---")
+      # SQLite does not have an explicit TRUNCATE command
+      # However, DELETE w/o a where clause uses an optimization
+      # so it's fase.
+      # ref: https://www.sqlite.org/lang_delete.html#the_truncate_optimization
+      SensorReading.delete_all
+      Sensor.delete_all
+      SensorType.delete_all
+      ChecklistItem.delete_all
+      Checklist.delete_all
+      PlaceHierarchy.delete_all
+      Place.delete_all
+      ActiveRecord::Base.connection.execute("VACUUM;")
+    end
+
+    def seed_sensors!
+      Seeding::Seeder::Sensors.new.seed!(
+        sensors_per_place: DEFAULT_SENSORS_PER_PLACE,
+        log_method: method(:log),
+      )
     end
 
     def seed_checklists!
       log(" --- start (seeding checklists) ---")
-      Checklist.delete_all
 
       checklist_data = Place.pluck(:id).map do |place_id|
         {
@@ -53,11 +79,12 @@ class Seeding
     # If we switch from SQLite to Postgres we can skip a few steps here
     def seed_places!
       log(" --- start (seeding places) ---")
-      Place.delete_all
-
-      seed_corporations!(num_corporations_range: DEFAULT_NUM_CORPORATIONS)
-      corporation_places = Place.all.where.not(name: MONITOR_MAGIC_CORPORATION_NAME)
-      log("Seeded #{corporation_places.length} corporations")
+      mmagic_corporation_place = seed_mmagic_corporation!
+      seed_corporations!(
+        num_corporations_range: DEFAULT_NUM_CORPORATIONS,
+        mmagic_corporation_place: mmagic_corporation_place,
+      )
+      corporation_places = Place.all - [mmagic_corporation_place]
 
       seed_regions(
         corporation_places: corporation_places,
@@ -67,26 +94,27 @@ class Seeding
       # We can't simply use the records returned by seed_regions, because
       # they won't have id's following the bulk import. This is a limitation
       # of SQLite & activerecord-import; would not be an issue with Postgres
-      region_places = Place.all - corporation_places
-      log("Seeded #{region_places.length} regions")
+      region_places = Place.all - corporation_places - [mmagic_corporation_place]
 
       seed_subregions(
         region_places: region_places,
         num_subregions_range: DEFAULT_NUM_SUBREGIONS_PER_REGION,
       )
-      subregion_places = Place.all - corporation_places - region_places
-      log("Seeded #{subregion_places.length} subregions")
+      subregion_places = Place.all - corporation_places - region_places - [mmagic_corporation_place]
 
       seed_units(
         subregion_places: subregion_places,
         num_units_range: DEFAULT_NUM_UNITS_PER_SUBREGION,
       )
-      total_place_count = Place.count
-      unit_place_count = total_place_count - corporation_places.length - region_places.length - subregion_places.length
-      log("Seeded #{unit_place_count} units")
+      unit_places = Place.all - corporation_places - region_places - subregion_places - [mmagic_corporation_place]
 
-      hierarchy_count = rebuild_hierarchy(region_places, subregion_places)
-      log("Rebuilt #{hierarchy_count} places_hierarchies for #{total_place_count} places")
+      hierarchy_count = rebuild_hierarchy(
+        mmagic_corporation_place,
+        corporation_places,
+        region_places,
+        subregion_places,
+        unit_places,
+      )
     end
 
     private
@@ -96,46 +124,74 @@ class Seeding
     # and even more quickly because we use activerecord-import for a bulk insert
     # Now takes ~0.5sec for ~3K groups
     # TODO: needs tests
-    def rebuild_hierarchy(region_places, subregion_places)
-      PlaceHierarchy.delete_all
-      region_place_ids = region_places.map(&:id)
-      subregion_place_ids = subregion_places.map(&:id)
-      region_and_subregion_place_ids = (region_place_ids + subregion_place_ids)
-
-      unit_place_ids = Place.where.not(id: region_and_subregion_place_ids).pluck(:id)
+    def rebuild_hierarchy(mmagic_corporation_place, corporation_places, region_places, subregion_places, unit_places)
+      log(" --- rebuilding places hierarchy ---")
 
       parent_ids = Place.
         pluck(:id, :parent_id).
         each_with_object({}) { |x, memo| memo[x[0]] = x[1] }
 
-      stuff = []
+      # ROOT
+      # we don't need to insert it, because we inserted the root
+      # mmagic place "normally" and not via bulk insert
 
-      stuff << unit_place_ids.map do |unit_place_id|
-        parent_id = parent_ids[unit_place_id]
-        grandparent_id = parent_ids[parent_ids[unit_place_id]]
-        me = { descendant_id: unit_place_id }
+      # CORPORATIONS
+      stuff = corporation_places.map do |corporation_place|
+        me = { descendant_id: corporation_place.id }
         [
-          { **me, ancestor_id: unit_place_id, generations: 0 },
-          { **me, ancestor_id: parent_id, generations: 1 },
-          { **me, ancestor_id: grandparent_id, generations: 2 },
+          { **me, ancestor_id: corporation_place.id, generations: 0 },
+          { **me, ancestor_id: mmagic_corporation_place.id, generations: 0 },
         ]
       end
 
-      stuff << subregion_place_ids.map do |subregion_place_id|
-        me = { descendant_id: subregion_place_id }
+      # REGIONS
+      stuff << region_places.map do |region_place|
+        id = region_place.id
+        corporation_parent_id = parent_ids[id]
+        me = { descendant_id: id }
         [
-          { **me, ancestor_id: subregion_place_id, generations: 0 },
-          { **me, ancestor_id: parent_ids[subregion_place_id], generations: 1 },
+          { **me, ancestor_id: id, generations: 0 },
+          { **me, ancestor_id: corporation_parent_id, generations: 1 },
+          { **me, ancestor_id: mmagic_corporation_place.id, generations: 2 },
         ]
       end
 
-      stuff << region_place_ids.map do |region_place_id|
-        { ancestor_id: region_place_id, descendant_id: region_place_id, generations: 0 }
+      # SUBREGIONS
+      stuff << subregion_places.map do |subregion_place|
+        id = subregion_place.id
+        region_parent_id = parent_ids[id]
+        corporation_grandparent_id = parent_ids[parent_ids[id]]
+        me = { descendant_id: id }
+        [
+          { **me, ancestor_id: id, generations: 0 },
+          { **me, ancestor_id: region_parent_id, generations: 1 },
+          { **me, ancestor_id: corporation_grandparent_id, generations: 2 },
+          { **me, ancestor_id: mmagic_corporation_place.id, generations: 3 },
+        ]
       end
-      ActiveRecord::Base.logger.silence do
+
+      # UNITS
+      stuff << unit_places.map do |unit_place|
+        id = unit_place.id
+        subregion_parent_id = parent_ids[id]
+        region_grandparent_id = parent_ids[parent_ids[id]]
+        corporation_ggrandparent_id = parent_ids[parent_ids[parent_ids[id]]]
+        me = { descendant_id: id }
+        [
+          { **me, ancestor_id: id, generations: 0 },
+          { **me, ancestor_id: subregion_parent_id, generations: 1 },
+          { **me, ancestor_id: region_grandparent_id, generations: 2 },
+          { **me, ancestor_id: corporation_ggrandparent_id, generations: 3 },
+          { **me, ancestor_id: mmagic_corporation_place.id, generations: 4 },
+        ]
+      end
+
+      result = ActiveRecord::Base.logger.silence do
         PlaceHierarchy.import(stuff.flatten)
       end
-      stuff.flatten.length
+      raise if result.failed_instances.any?
+
+      PlaceHierarchy.count
     end
 
     def log(msg)
@@ -146,7 +202,7 @@ class Seeding
       elapsed_since_last = Time.current - @last_log_time
       elapsed_total_formatted = sprintf("%2.2f", elapsed_total).rjust(5, " ")
       elapsed_since_last_formatted = sprintf("%2.2f", elapsed_since_last).rjust(5, " ")
-      Rails.logger.info "#{self.class.name} #{caller_method}##{lineno} "\
+      Rails.logger.info "#{self.class.name} #{caller_method}:#{lineno} "\
                         "#{elapsed_total_formatted}s /#{elapsed_since_last_formatted}s | "\
                         "#{msg}"
 
@@ -157,11 +213,15 @@ class Seeding
       rand(1..100)
     end
 
-    def seed_corporations!(num_corporations_range:)
-      mmagic_corporation_place = Place.create(
+    def seed_mmagic_corporation!
+      Place.create(
         name: "Monitor Magic, Inc.",
         place_type: :corporation,
       )
+    end
+
+    def seed_corporations!(num_corporations_range:, mmagic_corporation_place:)
+      log(" --- seeding corporations ---")
 
       corporation_places = rand(num_corporations_range).times.map do
         {
@@ -171,13 +231,15 @@ class Seeding
         }
       end
 
+      log("importing #{corporation_places.length} corporation_places")
       ActiveRecord::Base.logger.silence do
         Place.import(corporation_places)
       end
     end
 
     def seed_regions(corporation_places:, num_regions_per_corporation_range:)
-      regions = corporation_places.each.map do |corporation_place|
+      log(" --- start (seeding regions) ---")
+      regions = corporation_places.map do |corporation_place|
         num_regions = rand(num_regions_per_corporation_range)
         region_names = Seeding::Text::REGIONS_AND_SUBREGIONS.keys.sample(num_regions)
         region_names.map do |region_name|
@@ -187,13 +249,16 @@ class Seeding
             parent_id: corporation_place.id,
           }
         end
-      end
+      end.flatten
+
+      log("importing #{regions.length} regions")
       ActiveRecord::Base.logger.silence do
-        Place.import(regions.flatten)
+        Place.import(regions)
       end
     end
 
     def seed_subregions(region_places:, num_subregions_range:)
+      log(" --- start (seeding subregions) ---")
       subregion_places = region_places.map do |region|
         num_subregions = rand(num_subregions_range)
 
@@ -211,6 +276,7 @@ class Seeding
         end
       end.flatten
 
+      log("importing #{subregion_places.length} subregions")
       ActiveRecord::Base.logger.silence do
         Place.import(subregion_places)
       end
@@ -220,6 +286,7 @@ class Seeding
     #       for ~3K names. Could probably precalculate 50,000 names,
     #       store them in a file, randomly select from there?
     def seed_units(subregion_places:, num_units_range:)
+      log(" --- start (seeding units) ---")
       unit_places = subregion_places.map do |subregion_place|
         rand(num_units_range).times.map do
           {
@@ -230,6 +297,7 @@ class Seeding
         end
       end.flatten
 
+      log("importing #{unit_places.length} unit_places")
       ActiveRecord::Base.logger.silence do
         Place.import(unit_places, batch_size: BATCH_IMPORT_SIZE)
       end
